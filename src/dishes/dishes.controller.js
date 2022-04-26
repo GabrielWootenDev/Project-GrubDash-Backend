5c621e66de721b79088697c4a7633c12a4724fc7
Gabriel <wootengabriel90@gmail.com>
2022-04-26 18:04:33 -0400

    progress on backend for /dishes nearly complete

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,95 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function read(req, res) {
+  const foundDish = res.locals.dish;
+  res.json({ data: foundDish });
+}
+
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    next();
+  }
+
+  next({
+    status: 404,
+    message: `Dish id not found: ${dishId}`,
+  });
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    data[propertyName]
+      ? next()
+      : next({ status: 400, message: `Dish must include a ${propertyName}` });
+  };
+}
+
+function validatePrice(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  Number.isInteger(price) && price > 0
+    ? next()
+    : next({
+        status: 400,
+        message: `Dish must have a price that is an integer greater than 0`,
+      });
+}
+
+function create(req, res) {
+  const {
+    data: { name, description, price, image_url },
+  } = req.body;
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function update(req, res) {
+  const foundDish = res.locals.dish;
+  const {
+    data: { name, description, price, image_url },
+  } = req.body;
+
+  foundDish.name = name;
+  foundDish.description = description;
+  foundDish.price = price;
+  foundDish.image_url = image_url;
+
+  res.json({ data: foundDish });
+}
+
+module.exports = {
+  list,
+  read: [dishExists, read],
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("image_url"),
+    validatePrice,
+    create,
+  ],
+  update: [
+    dishExists,
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("image_url"),
+    validatePrice,
+    update,
+  ],
+};
